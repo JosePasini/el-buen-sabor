0cc9ae72cd7b3dd2583fafe30b5dac4d4d4003a6
Jose Pasini <jose.pasini@mercadolibre.com>
2022-06-22 00:46:30 -0300

    db config

--- internal/app/config.go
+++ internal/app/config.go
@@ -15,9 +15,9 @@ func NewConfig(scope string) (instrumentos.AppConfig, error) {
 
 	godotenv.Load()
 
-	HOST_ENV := os.Getenv("DB_HOST")
 	USER_ENV := os.Getenv("DB_USER")
 	PASS_ENV := os.Getenv("DB_PASS")
+	HOST_ENV := os.Getenv("DB_HOST")
 	NAME_ENV := os.Getenv("DB_NAME")
 
 	fmt.Println(":::::", HOST_ENV, " ", USER_ENV, " ", PASS_ENV, " ", NAME_ENV)
@@ -35,10 +35,18 @@ func NewConfig(scope string) (instrumentos.AppConfig, error) {
 
 	return instrumentos.AppConfig{
 		DB: database.MySQLConfig{
-			User:     "baac4c3d3bb29e",
-			Password: "408399dd",
-			Host:     "us-cdbr-east-05.cleardb.net",
-			Database: "heroku_9952cf2f0b46460",
+			User:     USER_ENV,
+			Password: PASS_ENV,
+			Host:     HOST_ENV,
+			Database: NAME_ENV,
 		},
 	}, nil
+	// return instrumentos.AppConfig{
+	// 	DB: database.MySQLConfig{
+	// 		User:     "baac4c3d3bb29e",
+	// 		Password: "408399dd",
+	// 		Host:     "us-cdbr-east-05.cleardb.net",
+	// 		Database: "heroku_9952cf2f0b46460",
+	// 	},
+	// }, nil
 }
